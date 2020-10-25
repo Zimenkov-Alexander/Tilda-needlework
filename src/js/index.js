516358d52ea8c587af455399407addd23264f794
Alex <ZimenkovIUAI@gmail.com>
2020-10-25 19:31:16 +0200

    release desktop version

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import modalForm from './modules/modal-form';
-import modalThankWindow from './modules/modal-thank-window';
-
-window.addEventListener('DOMContentLoaded', () => {
-	modalForm('.modal--form','.close__modal', '.btn');
-	modalThankWindow('.modal--thank-window', '.btn--form-modal');
-});
